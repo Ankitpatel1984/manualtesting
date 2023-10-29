edf64e17efbf99f29929c5ddf7cac72a75bfa042
Ankit Patel <ankitpatel1984@gmail.com>
2023-10-29 16:22:30 +0000

    Manual Testing

---
+++ ManualTesting.java
@@ -0,0 +1,28 @@
+package manualtesting;
+
+public class ManualTesting {
+
+    public static void main(String[] args) {
+        System.out.println("What is Software Testing");
+        System.out.println("What are the Benefits of Software Tester");
+        System.out.println("What is the Qualification of Software Tester");
+        System.out.println("What are the Challenges of Software Tester");
+        System.out.println("What is Blackbox Testing");
+        System.out.println("Do you have understanding of SDLC");
+        System.out.println("Principles of software testing");
+        System.out.println("Do you create test plan");
+        System.out.println("As a software tester which kind of documents are you creating");
+        System.out.println("Challenges of software tester");
+        System.out.println("who create the test plan");
+        System.out.println("what is manual testing");
+        System.out.println("can you please tell me the testing types");
+        System.out.println("can you please tell me difference between smoke test and sanity test ");
+        System.out.println("who decide severity and priority");
+        System.out.println("who does testing");
+        System.out.println("what are the steps in SDLC");
+        System.out.println("how many environment available at your work place");
+    }
+
+
+}
+
